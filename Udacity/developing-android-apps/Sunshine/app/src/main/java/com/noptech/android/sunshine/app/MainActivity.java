55c2c5a87a18de32bba335d585e943bef2d449ef
Mikkel Madsen <m@madsn.net>
2015-04-07 20:46:50 +0200

    Lesson 1 completed

--- Udacity/developing-android-apps/Sunshine/app/src/main/java/com/noptech/android/sunshine/app/MainActivity.java
+++ Udacity/developing-android-apps/Sunshine/app/src/main/java/com/noptech/android/sunshine/app/MainActivity.java
@@ -10,6 +10,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -60,6 +65,23 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            List<String> entries = new ArrayList<String>();
+            entries.add("Today - Sunny - 10/15");
+            entries.add("Tomorrow - Sunny - 10/15");
+            entries.add("Thursday - Sunny - 10/15");
+            entries.add("Friday - Sunny - 10/15");
+            entries.add("Saturday - Sunny - 10/15");
+            entries.add("Sunday - Sunny - 10/15");
+            entries.add("Monday - Sunny - 10/15");
+            entries.add("Tuesday - Sunny - 10/15");
+            entries.add("Wednesday - Sunny - 10/15");
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, entries);
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(adapter);
+
             return rootView;
         }
     }
