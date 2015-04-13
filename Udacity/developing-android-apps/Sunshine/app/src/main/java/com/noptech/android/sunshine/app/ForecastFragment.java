111f59984caa3d1d4a34764ba8dd9f67332d526b
Mikkel Madsen <m@madsn.net>
2015-04-13 22:49:34 +0200

    Lesson 2 continued

--- Udacity/developing-android-apps/Sunshine/app/src/main/java/com/noptech/android/sunshine/app/ForecastFragment.java
+++ Udacity/developing-android-apps/Sunshine/app/src/main/java/com/noptech/android/sunshine/app/ForecastFragment.java
@@ -3,6 +3,9 @@ package com.noptech.android.sunshine.app;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -14,6 +17,30 @@ import java.util.List;
 public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                FetchWeatherTask task = new FetchWeatherTask();
+                task.execute();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
